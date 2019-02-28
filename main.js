dd19453cd7e9f1cd699ca3356097927c9589cef5
Eric Biersner <eric.biersner@gmail.com>
2019-02-28 12:41:14 -0800

    add about, contact form, menu download

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-document.getElementById('isOpen')
+
+var index = 0; 		
+var images = ["img/brew1.jpg","img/brew2.jpg","img/brew5.jpg"];	
+
+
+function changeImg(){
+    document.carousel.src = images[index];
+
+    index = (index + 1) % images.length;
+    setTimeout("changeImg()", 3500); 
+}
+
+window.onload = function() {
+
+    // image carousel
+    changeImg();
+
+    // are open
+    var open = document.getElementById('isOpen');
+    var date = new Date();
+    var time = date.getHours();
+    console.log(time)
+
+    if(time > 3 && time < 11){
+        open.innerText = "Closed"
+    } else {
+        open.innerText = "Open"
+    }
+
+}
